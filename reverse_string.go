8a3c67104625bf955c944cd062e2f1aa147e3822
Prem Kumar <prem_kumar@epam.com>
2022-05-17 16:44:39 -0400

    solution commit

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	//Place your code here
+	byte_str := []rune(input)
+	for i := len(byte_str)/2 - 1; i >= 0; i-- {
+		rev := len(byte_str) - 1 - i
+		byte_str[i], byte_str[rev] = byte_str[rev], byte_str[i]
+	}
+	output= string(byte_str)
 	return output
 }
